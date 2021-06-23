53ab45d841fdfb93999138c25fe98277c9dacca3
= <=>
2021-06-23 10:10:25 +0700

    Wed change

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
 const btnScrollTO = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+///////////////////////////////////////
+
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -31,6 +33,7 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+//Button scrolling
 btnScrollTO.addEventListener('click', e => {
   const s1coords = section1.getBoundingClientRect();
   // window.scrollTo({
@@ -41,31 +44,74 @@ btnScrollTO.addEventListener('click', e => {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
-const h1 = document.querySelector('h1');
-
 // h1.addEventListener('mouseenter', e => {
 //   alert('You are hovering the header');
 // });
 
-const alert1 = e => {
-  alert('you are hovering the header');
-  h1.removeEventListener('mouseenter', alert1);
-};
-h1.addEventListener('mouseenter', alert1);
+//page navigation
 
-setTimeout(() => h1.removeEventListener('mouseenter', alert1), 3000);
+//1. Add event listener to common parent element
+//2. determine what element originated the envet
 
-const randomInt = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1) + min);
-const randomColor = () =>
-  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
-
-document.querySelector('.nav__link').addEventListener('click', function (e) {
-  this.style.color = randomColor();
-});
 document.querySelector('.nav__links').addEventListener('click', function (e) {
-  this.style.backgroundColor = randomColor();
+  e.preventDefault();
+
+  //matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
 });
-document.querySelector('.nav').addEventListener('click', function (e) {
-  console.log('clicked');
+const h1 = document.querySelector('h1');
+
+h1.firstElementChild.style.color = 'red';
+h1.lastElementChild.style.color = 'red';
+
+[...h1.parentElement.children].forEach(el => {
+  if (el !== h1) {
+    el.style.color = 'red';
+  }
 });
+
+//tabbed component
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  if (!clicked) return;
+
+  //remove tabs
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(t => t.classList.remove('operations__content--active'));
+
+  //activate tabs
+  clicked.classList.add('operations__tab--active');
+
+  //activate content area
+  console.log(clicked.dataset.tab);
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+//menu fade animation
+const nav = document.querySelector('.nav');
+
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const sibling = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    sibling.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+nav.addEventListener('mouseover', handleHover.bind(0.4));
+nav.addEventListener('mouseout', handleHover.bind(1));
